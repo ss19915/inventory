e817a072f1fee9137d755994ba5e391ebd4091c4
Suraj <surajsharma571994@gmail.com>
2021-04-04 03:18:05 +0530

    .

--- packages/ui/src/components/CreateProduct/CreateProduct.js
+++ packages/ui/src/components/CreateProduct/CreateProduct.js
@@ -1,43 +1,71 @@
 import {
-    Typography,
-    TextField,
-    InputLabel,
-    TextareaAutosize,
-    Button,
-    Grid,
- } from '@material-ui/core/';
-
+  Typography,
+  TextField,
+  Button,
+  Grid,
+  Paper,
+} from "@material-ui/core/";
 
 const CreateProduct = (props) => {
-    const {
-        onInputChange,
-        onAdd,
-        isAddDisabled,
-    } = props;
+  const { onInputChange, onAdd, isAddDisabled } = props;
 
-    return (
-        <Grid container direction='column' spacing={2}>
-            <Grid item>
-                <Typography variant='h5'>Create Product</Typography>
-                <TextField onChange={onInputChange} id='Name' label='Name'/>
-                <TextField onChange={onInputChange} id='Price' type='number' label='Price($)'/>
-                <TextField onChange={onInputChange} id='Rating' type='number' label='Rating'/>
-            </Grid>
-            <Grid item>
-                <InputLabel>Description</InputLabel>
-                <TextareaAutosize
-                    onChange={onInputChange}
-                    rowsMin={3}
-                    id='Description'
-                    label='Description'
-                    placeholder='Product Description'
-                />
-            </Grid>
-            <Grid item>
-                <Button disabled={isAddDisabled} onClick={onAdd}>Add</Button>
-            </Grid>
+  return (
+    <Paper style={{ padding: "1em" }}>
+      <Grid container spacing={2} justify="center">
+        <Grid item>
+          <Typography variant="h5">Create Product</Typography>
         </Grid>
-    );
-}
+        <Grid item xs={11} style={{ borderTop: "1px solid #DDD" }}></Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            onChange={onInputChange}
+            id="Name"
+            label="Name"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            fullWidth
+            onChange={onInputChange}
+            id="Price"
+            type="number"
+            label="Price($)"
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            fullWidth
+            onChange={onInputChange}
+            id="Rating"
+            type="number"
+            label="Rating"
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            multiline
+            fullWidth
+            onChange={onInputChange}
+            rows={5}
+            id="Description"
+            label="Description"
+          />
+        </Grid>
+        <Grid item xs={9}></Grid>
+        <Grid item xs={3}>
+          <Button
+            fullWidth
+            variant="outlined"
+            disabled={isAddDisabled}
+            onClick={onAdd}
+          >
+            + Add
+          </Button>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
 
 export default CreateProduct;
--- packages/ui/src/components/Views/AddProductView.js
+++ packages/ui/src/components/Views/AddProductView.js
@@ -1,14 +1,19 @@
-import CreateProduct from '../CreateProduct';
-import { withRouter } from 'react-router-dom';
+import CreateProduct from "../CreateProduct";
+import { withRouter } from "react-router-dom";
+import { Grid } from "@material-ui/core";
 
 const AddProductView = ({ history }) => {
-    const onProductCreate= () => {
-        history.push('/');
-    }
+  const onProductCreate = () => {
+    history.push("/");
+  };
 
-    return (
-        <CreateProduct onProductCreate={onProductCreate}/>
-    );
-}
+  return (
+    <Grid container justify='center'>
+      <Grid item xs={5}>
+        <CreateProduct onProductCreate={onProductCreate} />
+      </Grid>
+    </Grid>
+  );
+};
 
 export default withRouter(AddProductView);
